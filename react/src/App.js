237dc74a5e8097d797846c469a705e10a624c650
Zachary Dewitt-Moran <ZPMoran@bemis.com>
2019-09-26 11:15:49 -0500

    Fixed App.js to be a function instead of a class, Fixed saga action

--- react/src/App.js
+++ react/src/App.js
@@ -1,22 +1,20 @@
+
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import {alertMessage,sagaStart} from './Actions/globalActions'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    var message;
-  }
-  render() {
+function App(props)
+{
+
   return (
     <div className="App">
-      <h1 onClick={() => this.props.alertMessage(this.props.message)}> Click Default Alert Action</h1>
-      <h1 onClick={() => this.props.sagaStart()}> Click Default Saga Action</h1>
-      <h1>Result: {this.props.result} </h1>
+      <h1 onClick={() => props.alertMessage(props.message)}> Click Default Alert Action</h1>
+      <h1 onClick={() => props.sagaStart()}> Click Default Saga Action</h1>
+      <h1>Result: </h1>
+      {JSON.stringify(props.result)}
     </div>
   );
-}
 }
 //THIS FUNCTION MAPS STORE TO STATE
 const mapState = state => ({
@@ -35,3 +33,6 @@ export default connect(
   mapState,
   mapDispatch
 )(App);
+
+
+
